e624f98d7cb134548114c3ea98d7af098aa3bcea
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2020-04-15 20:14:32 -0300

    output to file

--- src/main/java/ar/edu/itba/ss/App.java
+++ src/main/java/ar/edu/itba/ss/App.java
@@ -1,15 +1,14 @@
 package ar.edu.itba.ss;
 
-
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
 
     private static double dt;
-    private static double time;
-    private static double fps;
     private static final int SUN_ID = 0;
     private static double G = 6.673 * Math.pow(10, -11);
     private static final double AU = 149598073;
@@ -17,7 +16,6 @@ public class App {
     private static final double SPACESHIP_DISTANCE = 1500000;
     private static final double SPACESHIP_SPEED = 8000;
     private static final double SPACESHIP_MASS = 2 * Math.pow(10,5);
-
     // Mars
     private static final double MARS_MASS = 6.4171 * Math.pow(10,23);
     private static final double MARS_RADIUS = 3389500;
@@ -28,17 +26,27 @@ public class App {
     private static final double EARTH_RADIUS = 6371000;
 
     public static void main( String[] args ) {
+        File file = new File("output.txt");
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("output.xyz", "UTF-8");
+        } catch (Exception e) {
+            System.out.println("Couldn't write output to file...");
+            System.exit(1);
+        }
         Configuration configuration = CommandLineParser.parseCommandLine(args);
         List<Planet> planets = null;
         try {
             planets = FileParser.parseDynamicFile(configuration.getDynamicFile());
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
             System.out.println("Error reading dynamic file...");
+            System.exit(1);
         }
-        time = configuration.getTime();
+
+        double time = configuration.getTime();
+        double fps = configuration.getFps();
         dt = configuration.getDt();
-        fps = configuration.getFps();
+
 
         // Get earth-sun angle
         assert planets != null;
@@ -46,70 +54,93 @@ public class App {
         double earthSunAngle = getEarthSunAngle(earth);
         // Get earth-sun velocity angle
         double velocityAngle = getEarthSunVelocityAngle(earth);
-        // Check velocity with teacher?
-        double spaceshipX = earth.getX() + (SPACESHIP_DISTANCE + EARTH_RADIUS) * Math.cos(earthSunAngle);
-        double spaceshipY = earth.getY() + (SPACESHIP_DISTANCE + EARTH_RADIUS) * Math.sin(earthSunAngle);
-        double spaceshipVx = earth.getVx() + SPACESHIP_SPEED  * Math.cos(velocityAngle);
-        double spaceshipVy = earth.getVy() + SPACESHIP_SPEED * Math.sin(velocityAngle);
-        // Add Sun and Spaceship
-        planets.add(new Planet(SUN_ID, 0.0, 0.0, 0, 0, SUN_MASS, 0.2));
-        planets.add(new Planet(4, spaceshipX, spaceshipY, spaceshipVx, spaceshipVy, SPACESHIP_MASS, 0.08));
+        // TODO: Check velocity of spaceship
+        double spaceshipX = earth.x + (SPACESHIP_DISTANCE + EARTH_RADIUS) * Math.cos(earthSunAngle);
+        double spaceshipY = earth.y + (SPACESHIP_DISTANCE + EARTH_RADIUS) * Math.sin(earthSunAngle);
+        double spaceshipVx = earth.vx + SPACESHIP_SPEED  * Math.cos(velocityAngle);
+        double spaceshipVy = earth.vy + SPACESHIP_SPEED * Math.sin(velocityAngle);
 
-        earth.setMass(EARTH_MASS);
-        earth.setRadius(0.15);
+
+        // Add Sun and Spaceship
+        planets.add(new Planet(SUN_ID, 0.0, 0.0, 0, 0, SUN_MASS, 5));
+        planets.add(new Planet(4, spaceshipX, spaceshipY, spaceshipVx, spaceshipVy, SPACESHIP_MASS, 2));
+        earth.mass = EARTH_MASS;
+        earth.radius = 4;
         Planet mars = planets.get(1);
-        mars.setMass(MARS_MASS);
-        mars.setRadius(0.12);
+        mars.mass = MARS_MASS;
+        mars.radius = 3;
+
+        double minDistanceToMars = Double.POSITIVE_INFINITY;
+        double day = 0;
 
         List<Planet> oldPlanets = clonePlanets(planets);
         initializePlanets(planets, oldPlanets);
         int iterations = 0;
-        printPlanets(planets, iterations);
+        printPlanets(writer, planets, iterations++);
 
         int frame = 0;
-        for(double t = 0 ; t < time ; t += dt) {
+        for(double t = 0; t < time; t += dt) {
             oldPlanets = clonePlanets(planets);
             for (Planet p : planets) {
-                if (p.getId() != SUN_ID) {
+                if (p.id != SUN_ID) {
                     double[] force = force(p, oldPlanets);
-                    p.setAx(force[0]);
-                    p.setAy(force[1]);
 
-                    p.setX(p.getX() + p.getVx() * dt + (2.0/3) * p.getAx() * Math.pow(dt,2) - (1.0/6) * p.getPrevAx() *
-                            Math.pow(dt,2));
-                    p.setY(p.getY() + p.getVy() * dt + (2.0/3) * p.getAy() * Math.pow(dt,2) - (1.0/6) * p.getPrevAy() *
-                            Math.pow(dt,2));
+                    p.ax = force[0];
+                    p.ay = force[1];
+
+                    /* Beeman */
+                    p.x = p.x + p.vx * dt + (2.0 / 3) * p.ax * Math.pow(dt, 2) - (1.0 / 6) * p.prevAx * Math.pow(dt, 2);
+                    p.y = p.y + p.vy * dt + (2.0 / 3) * p.ay * Math.pow(dt, 2) - (1.0 / 6) * p.prevAy * Math.pow(dt, 2);
+
                 }
             }
             for (Planet p : planets) {
-                if (p.getId() != SUN_ID) {
+                if (p.id != SUN_ID) {
 
                     double[] newForce = force(p, planets);
                     double newAx = newForce[0];
                     double newAy = newForce[1];
 
-                    p.setVx(p.getVx() + (1.0 / 3) * newAx * dt + (5.0 / 6) * p.getAx() * dt - (1.0/6) * p.getPrevAx() * dt);
-                    p.setVy(p.getVy() + (1.0 / 3) * newAy * dt + (5.0 / 6) * p.getAy() * dt - (1.0/6) * p.getPrevAy() * dt);
-                    p.setPrevAx(p.getAx());
-                    p.setPrevAy(p.getPrevAy());
+                    p.vx = p.vx + (1.0 / 3) * newAx * dt + (5.0 / 6) * p.ax * dt - (1.0 / 6) * p.prevAx * dt;
+                    p.vy = p.vy + (1.0 / 3) * newAy * dt + (5.0 / 6) * p.ay * dt - (1.0 / 6) * p.prevAy * dt;
+
+                    p.prevAx = p.ax;
+                    p.prevAy = p.ay;
+
                 }
             }
-
+            double distanceToMars = calculateDistanceToMars(planets);
+            if(distanceToMars < minDistanceToMars) {
+                System.out.println("Entered here!");
+                day = t;
+                minDistanceToMars = distanceToMars;
+            }
             if(frame++ % fps == 0) {
-                printPlanets(planets, iterations++);
+                printPlanets(writer, planets, iterations++);
             }
         }
+        writer.close();
+        System.out.println("Tiempo total: " + time + " days");
+        System.out.println("Distancia minima a Marte: " + (minDistanceToMars/1000)/AU + "AU");
+        System.out.println(day + " dias despues de partir");
+    }
 
+
+    private static double calculateDistanceToMars(List<Planet> planets) {
+        Planet spaceship = planets.stream().filter(planet -> planet.id == 4).findAny().orElse(null);
+        Planet mars = planets.stream().filter(planet -> planet.id == 2).findAny().orElse(null);
+        return Math.sqrt(Math.pow(spaceship.x - mars.x, 2) + Math.pow(spaceship.y - mars.y, 2));
     }
 
     private static double getEarthSunAngle(Planet earth) {
+
         double earthSunAngle;
-        if (earth.getX() == 0) {
-            return Math.signum(earth.getY()) * Math.PI / 2;
+        if (earth.x == 0) {
+            return Math.signum(earth.y) * Math.PI / 2;
         }
         else{
-            earthSunAngle = Math.atan(earth.getY() / earth.getX());
-            if ((earth.getX() < 0 && earth.getY() > 0) || (earth.getX() < 0 && earth.getY() < 0)){
+            earthSunAngle = Math.atan(earth.y/earth.x);
+            if ((earth.x < 0 && earth.y > 0) || (earth.x < 0 && earth.y < 0)) {
                 earthSunAngle += Math.PI;
             }
             return earthSunAngle;
@@ -117,60 +148,64 @@ public class App {
     }
 
     private static double getEarthSunVelocityAngle(Planet earth) {
+
         double velocityAngle;
-        if (earth.getVx() == 0) {
-            return Math.signum(earth.getVy()) * Math.PI / 2;
+        if (earth.vx == 0) {
+            return Math.signum(earth.vy) * Math.PI / 2;
         }
         else {
-            velocityAngle = Math.atan(earth.getVy() / earth.getVx());
-            if ((earth.getVx() < 0 && earth.getVy() > 0) || (earth.getVx() < 0 && earth.getVy() < 0)) {
+            velocityAngle = Math.atan(earth.vy/earth.vx);
+            if ((earth.vx < 0 && earth.vy > 0) || (earth.vx < 0 && earth.vy < 0)) {
                 velocityAngle += Math.PI;
             }
             return velocityAngle;
         }
     }
 
-
     private static void initializePlanets(List<Planet> planets, List<Planet> oldPlanets) {
         for (Planet p : planets){
-            if (p.getId() != SUN_ID) {
+            if (p.id != SUN_ID) {
                 firstStep(p, oldPlanets);
             }
         }
     }
 
-    private static void firstStep(Planet planet, List<Planet> planets) {
-        double[] force = force(planet, planets);
-        planet.setPrevAx(force[0]);
-        planet.setPrevAy(force[1]);
-        planet.setVx(planet.getVx() + dt * planet.getPrevAx());
-        planet.setVy(planet.getVy() + dt * planet.getPrevAy());
-        planet.setX(planet.getX() + dt * planet.getVx() + Math.pow(dt,2) * planet.getPrevAx());
-        planet.setY(planet.getY() + dt * planet.getVy() + Math.pow(dt,2) * planet.getPrevAy());
+    private static void firstStep(Planet p, List<Planet> planets) {
+        double[] force = force(p, planets);
+        p.prevAx = force[0];
+        p.prevAy = force[1];
+        p.vx = p.vx + dt * p.prevAx;
+        p.vy = p.vy + dt * p.prevAy;
+        p.x = p.x + dt * p.vx + Math.pow(dt, 2) * p.prevAx;
+        p.y = p.y + dt * p.vy + Math.pow(dt, 2) * p.prevAy;
     }
 
     private static double[] force (Planet p, List<Planet> oldPlanets){
         double[] force = {0,0};
         for (Planet otherPlanet : oldPlanets) {
-            if (p.getId() != otherPlanet.getId()) {
+            if (p.id != otherPlanet.id) {
+
                 double f = gravitationalForce(p, otherPlanet);
-                double dx = otherPlanet.getX() - p.getX();
-                double dy = otherPlanet.getY() - p.getY();
+
+                double dx = otherPlanet.x - p.x;
+                double dy = otherPlanet.y - p.y;
+
                 double mod = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy,2));
                 force[0] += f * (dx/mod);
                 force[1] += f * (dy/mod);
             }
         }
-        force[0] = force[0]/p.getMass();
-        force[1] = force[1]/p.getMass();
+        force[0] = force[0]/p.mass;
+        force[1] = force[1]/p.mass;
         return force;
     }
 
-    private static double gravitationalForce(Planet p1, Planet p2){
-        double distance = Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) + Math.pow(p1.getY() - p2.getY(), 2));
-        return G * (p1.getMass() * p2.getMass() / Math.pow(distance, 2));
+    private static double gravitationalForce(Planet p1, Planet p2) {
+        double distance = Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+        return G*(p1.mass*p2.mass/Math.pow(distance, 2));
     }
 
+    //checked
     private static List<Planet> clonePlanets(List<Planet> planets) {
         List<Planet> clones = new ArrayList<>();
         for (Planet p: planets){
@@ -184,14 +219,25 @@ public class App {
         return clones;
     }
 
-    private static void printPlanets(List<Planet> planets, int iterations) {
-        System.out.println(planets.size());
+    // checked
+    private static void printPlanets(PrintWriter writer, List<Planet> planets, int iterations) {
+        writer.println(planets.size());
+        writer.println(iterations);
+        for (Planet p : planets) {
+            double auX  = p.x / 1000;
+            auX = auX / AU;
+            double auY = p.y / 1000;
+            auY = auY / AU;
+            writer.println(p.id + "\t" + auX + "\t" + auY + "\t" + p.vx + "\t" + p.vy + "\t" + p.radius);
+        }
+
+        /*System.out.println(planets.size());
         System.out.println(iterations);
         for (Planet p : planets){
             double auX  = (p.getX() / 1000) / AU;
             double auY = (p.getY() / 1000) / AU;
             System.out.println(p.getId() + "\t" + auX + "\t" + auY + "\t" + p.getVx() + "\t" + p.getVy() + "\t" + p.getRadius());
-        }
+        }*/
     }
 
 }
--- src/main/java/ar/edu/itba/ss/Planet.java
+++ src/main/java/ar/edu/itba/ss/Planet.java
@@ -3,19 +3,19 @@ package ar.edu.itba.ss;
 
 public class Planet implements Cloneable{
 
-    private int id;
-    private double x;
-    private double y;
-    private double vx;
-    private double vy;
-    private double mass;
-    private double prevX;
-    private double prevY;
-    private double ax;
-    private double ay;
-    private double prevAx;
-    private double prevAy;
-    private double radius;
+    int id;
+    double x;
+    double y;
+    double vx;
+    double vy;
+    double mass;
+    double prevX;
+    double prevY;
+    double ax;
+    double ay;
+    double prevAx;
+    double prevAy;
+    double radius;
 
 
     Planet(int id, double x, double y, double vx, double vy, double mass, double radius) {
@@ -34,110 +34,6 @@ public class Planet implements Cloneable{
         this.y = y;
         this.vx = vx;
         this.vy = vy;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public double getVx() {
-        return vx;
-    }
-
-    public void setVx(double vx) {
-        this.vx = vx;
-    }
-
-    public double getVy() {
-        return vy;
-    }
-
-    public void setVy(double vy) {
-        this.vy = vy;
-    }
-
-    public double getMass() {
-        return mass;
-    }
-
-    public void setMass(double mass) {
-        this.mass = mass;
-    }
-
-    public double getPrevX() {
-        return prevX;
-    }
-
-    public void setPrevX(double prevX) {
-        this.prevX = prevX;
-    }
-
-    public double getPrevY() {
-        return prevY;
-    }
-
-    public void setPrevY(double prevY) {
-        this.prevY = prevY;
-    }
-
-    public double getAx() {
-        return ax;
-    }
-
-    public void setAx(double ax) {
-        this.ax = ax;
-    }
-
-    public double getAy() {
-        return ay;
-    }
-
-    public void setAy(double ay) {
-        this.ay = ay;
-    }
-
-    public double getPrevAx() {
-        return prevAx;
-    }
-
-    public void setPrevAx(double prevAx) {
-        this.prevAx = prevAx;
-    }
-
-    public double getPrevAy() {
-        return prevAy;
-    }
-
-    public void setPrevAy(double prevAy) {
-        this.prevAy = prevAy;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
-    public void setRadius(double radius) {
-        this.radius = radius;
     }
 
     Planet getClone() throws CloneNotSupportedException {
